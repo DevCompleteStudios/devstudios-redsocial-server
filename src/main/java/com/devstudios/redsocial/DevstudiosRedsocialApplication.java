e010591f231628261d1ab7849b07eb3fbf5da27a
Jorge Yael Chino Bolaños <jorge.yael333@gmail.com>
2024-12-17 15:08:49 -0600

    First commit

---
+++ src/main/java/com/devstudios/redsocial/DevstudiosRedsocialApplication.java
@@ -0,0 +1,13 @@
+package com.devstudios.redsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevstudiosRedsocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevstudiosRedsocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devstudios/redsocial/devstudios_redsocial/DevstudiosRedsocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.redsocial.devstudios_redsocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevstudiosRedsocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
